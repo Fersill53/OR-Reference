f9d855a3b4d7bdec8e6680735d2e67aca44721ab
Fernando Siller <fernando@Fernandos-MBP.home.local>
2025-11-06 20:28:57 -0600

    fixed router issue in app.comp.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from "@angular/core";
+/*import { Component } from "@angular/core";
 
-import { RouterOutlet } from "@angular/router";
+import { RouterOutlet, RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, RouterLink],
     template: `<router-outlet></router-outlet>`
 })
 
+export class AppComponent {} */
+
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <nav style="padding:12px;border-bottom:1px solid #eee">
+      <a routerLink="/procedures">Open Procedures</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
+})
 export class AppComponent {}
