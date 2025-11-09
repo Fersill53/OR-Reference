796bd851f6ac7855e6d3638bee794658f4f156ae
Fernando Siller <fernando@Fernandos-MBP.home.local>
2025-11-08 22:17:39 -0600

    replacing old files

--- client/src/app/features/procedures/procedure-detail/procedure-detail.component.ts
+++ client/src/app/features/procedures/procedure-detail/procedure-detail.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component } from "@angular/core";
 
 import { CommonModule } from "@angular/common";
@@ -28,4 +29,30 @@ export class ProcedureDetailComponent {
             switchMap(params => this.svc.getById(params.get('id')!))
         );
     }
+}
+    */
+
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, ActivatedRoute } from "@angular/router";
+import { ProceduresService } from "../../../core/services/procedures.service";
+import { Procedure } from "../../../core/models/procedure.model";
+import { switchMap } from "rxjs/operators";
+import { Observable } from "rxjs";
+
+@Component({
+  selector: 'app-procedure-detail',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './procedure-detail.component.html',
+  styleUrls: ['./procedure-detail.component.scss']
+})
+export class ProcedureDetailComponent {
+  readonly procedure$!: Observable<Procedure | undefined>;
+
+  constructor(private route: ActivatedRoute, private svc: ProceduresService) {
+    this.procedure$ = this.route.paramMap.pipe(
+      switchMap(params => this.svc.getById(params.get('id')!))
+    );
+  }
 }
